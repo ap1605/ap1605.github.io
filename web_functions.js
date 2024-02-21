36c63bdc20465374cbfcf034436554bcb71814e7
ap1605 <amp322@pitt.edu>
2024-02-21 01:29:20 -0600

    game hasnew ui to match rest of page a bit better

--- web_functions.js
+++ web_functions.js
@@ -9,21 +9,23 @@ function openNav(x) {
   var mPage = document.getElementById("main");
   var head = document.getElementById("headImg");
   var navBtn = document.getElementById("sideNavBtn");
+  var first_game = document.getElementById("2DGame");
 
   var backColor = document.body.style.backgroundColor;
   if(sNav.style.width > "0px"){
     sNav.style.width = "0px";
     mPage.style.marginLeft = "0px";
     head.style.marginLeft = "0px";
-    navBtn.style.marginLeft = "0px"
+    navBtn.style.marginLeft = "0px";
+    first_game.style.marginLeft = "0px";
     backColor = "white";
-
   }
   else{
     sNav.style.width = "250px";
     mPage.style.marginLeft = "250px";
     head.style.marginLeft = "250px";
     navBtn.style.marginLeft = "-250px";
+    first_game.style.marginLeft = "250px";
     backColor = "rgba(0,0,0,0.4)";
   }
   x.classList.toggle("change");
